3d70fd20833f9eefd6e5b90fb267cf060f4f799c
nikguin04 <niklaspj2004@gmail.com>
2025-02-04 17:24:56 +0100

    USB switch from CDC to HID

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -54,7 +54,6 @@ static void MX_GPIO_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 int _write(int file, char *ptr, int len) {
-	CDC_Transmit_FS(ptr, len);
     return len;
 }
 /* USER CODE END 0 */
